fe523f96672aa1fdcc63ffad9afb2521051fd889
Guilherme Torres <guiatorres8@gmail.com>
2025-02-25 21:14:30 -0300

    start express routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
+const port = 3333;
 
-app.get('/users', (req, res)=>{
+app.use(express.json())
 
-  res.send('Hello World!')
+const users = ["Diego", "Guilherme", "Robson", "Pedro"];
+
+app.get("/users", (req, res) => {
+  const search = String(req.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
   
+	res.json(filteredUsers);
 });
 
-app.listen(3333); 
+app.get("/users/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const user = users[id];
+  res.json(user)
+});
+
+app.post("/users", (req, res) => {
+	const data = req.body;
+  const user = {
+    "name": 
+  }
+
+  console.log(data);
+
+  res.json(data)
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
