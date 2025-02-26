042fb2091135767c8f83e7bb072667295220b1a5
Guilherme Torres <guiatorres8@gmail.com>
2025-02-25 22:12:41 -0300

    connect to database file

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,11 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 const port = 3333;
 
-app.use(express.json())
-
-const users = ["Diego", "Guilherme", "Robson", "Pedro"];
-
-app.get("/users", (req, res) => {
-  const search = String(req.query.search);
-
-  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-  
-	res.json(filteredUsers);
-});
-
-app.get("/users/:id", (req, res) => {
-	const id = Number(req.params.id);
-	const user = users[id];
-  res.json(user)
-});
-
-app.post("/users", (req, res) => {
-	const data = req.body;
-  const user = {
-    "name": 
-  }
-
-  console.log(data);
-
-  res.json(data)
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
