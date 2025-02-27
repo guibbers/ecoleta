55961de45ead7e39f11d46429e2edd6875e8fd8d
Guilherme Torres <guiatorres8@gmail.com>
2025-02-27 00:06:29 -0300

    add imagems & code list items route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import knex from './database/connection'
 
 const routes = express.Router();
-routes.get('/', (req, res)=>{
-  res.json({
-    message: 'hello world'
-  });
+
+// LIST ALL ITEMS
+routes.get('/items', async (req, res)=>{
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item =>{
+    return {
+      title: item.name,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  res.json(serializedItems)
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from 'node:path';
 import routes from "./routes";
 
 const app = express();
@@ -6,6 +7,8 @@ const port = 3333;
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
