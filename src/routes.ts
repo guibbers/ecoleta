a3cc19db86ebcf26616bc8e563e81e87728011cf
Guilherme Torres <guiatorres8@gmail.com>
2025-02-28 00:07:15 -0300

    work on routes

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ routes.get('/items', async (req, res)=>{
 
   const serializedItems = items.map(item =>{
     return {
+      id: item.id,
       title: item.name,
       image_url: `http://localhost:3333/uploads/${item.image}`
     }
@@ -16,5 +17,41 @@ routes.get('/items', async (req, res)=>{
 
   res.json(serializedItems)
 });
+
+// CREATE COLLECT POINT
+routes.post('/points', async (req, res)=>{
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+  const ids = await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const pointItems = items.map((item_id: number) =>{
+    return {
+      item_id,
+      point_id: ids[0]
+    }
+  });
+
+  await knex('point_items').insert(pointItems);
+
+  res.json({ success: true});
+})
 
 export default routes;
