b1596612a98ecb8256db0faa8b596dd93bb9df40
Guilherme Torres <guiatorres8@gmail.com>
2025-03-01 23:18:56 -0300

    backend beta done

--- src/controllers/pointsController.ts
+++ src/controllers/pointsController.ts
@@ -25,7 +25,7 @@ class PointsController {
 
 		const trx = await knex.transaction();
     const point = {
-			image: "image-fake",
+			image: "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?q=60&w=1974&auto=format&fit=crop&w=400ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 			name,
 			email,
 			whatsapp,
