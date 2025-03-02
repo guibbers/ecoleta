f61414f9720dcf805a04e52d54775e0ff157a8c3
Guilherme Torres <guiatorres8@gmail.com>
2025-03-01 23:06:45 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from "express";
 import path from 'node:path';
 import routes from "./routes";
@@ -5,6 +6,7 @@ import routes from "./routes";
 const app = express();
 const port = 3333;
 
+app.use(cors({}));
 app.use(express.json());
 app.use(routes);
 
