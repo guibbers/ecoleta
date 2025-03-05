4de4e52062d1450854909acec9e8d7736bfbcbcb
Guilherme Torres <guiatorres8@gmail.com>
2025-03-04 23:48:38 -0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import './App.css';
-
-import Header from './Header';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <h1>Conteúdo da aplicação</h1>
-    </div>
-  );
+
+	return (
+		<h1>Hello world</h1>
+	);
 }
 
 export default App;
