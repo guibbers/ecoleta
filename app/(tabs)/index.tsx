1a9bc21aa04b5a384023a88d73a976c1ec7e160e
Guilherme Torres <guiatorres8@gmail.com>
2025-03-20 00:10:03 -0300

    add fonts & splash screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,46 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import Home from '@/src/pages/Home';
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
 export default function HomeScreen() {
+
+  const [appReady, setAppReady] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+        await Font.loadAsync({
+          "RobotoRegular": Roboto_400Regular,
+          "RobotoMNedium": Roboto_500Medium,
+          "UbuntuBold": Ubuntu_700Bold,
+        })
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppReady(true)
+      }
+    })()
+  }, [])
+
+  const onLayout = useCallback(() => {
+    if (appReady) {
+      SplashScreen.hideAsync()
+    }
+  }, [appReady])
+
+  if (!appReady) {
+    return null
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
