156a5e4ea64f3a66fdc06bb3b10dfcf7415f45ed
Guilherme Torres <guiatorres8@gmail.com>
2025-03-28 13:02:43 -0300

    finish image upload

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,38 +1,41 @@
-import React, { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
-import { FiUpload } from 'react-icons/fi'
+import type React from "react";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
 
-import './styles.css'
+import "./styles.css";
 
-function Dropzone() {
-  const [selectedFileUrl, setSelectedFileurl] = useState('')
-
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    const file = acceptedFiles[0];
-    const fileUrl = URL.createObjectURL(file)
-    setSelectedFileurl(fileUrl);
-    
-  }, [])
-  const {getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: {'image/*': []}
-  })
-
-  return (
-    <div className='dropzone' {...getRootProps()}>
-      <input {...getInputProps()} accept='image/*' />
-
-      { selectedFileUrl 
-          ? <img src={selectedFileUrl} alt="Point Thumbnail"/>
-          : (
-            <p>
-            <FiUpload />
-            Imagem do estabelecimento
-          </p>
-          )
-      }
-          
-    </div>
-  )
+interface Props {
+	onFileUploaded: (file: File) => void;
 }
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
+	const [selectedFileUrl, setSelectedFileurl] = useState("");
+
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		const file = acceptedFiles[0];
+		const fileUrl = URL.createObjectURL(file);
+		setSelectedFileurl(fileUrl);
+    onFileUploaded(file);
+	}, [onFileUploaded]);
+	const { getRootProps, getInputProps } = useDropzone({
+		onDrop,
+		accept: { "image/*": [] },
+	});
+
+	return (
+		<div className="dropzone" {...getRootProps()}>
+			<input {...getInputProps()} accept="image/*" />
+
+			{selectedFileUrl ? (
+				<img src={selectedFileUrl} alt="Point Thumbnail" />
+			) : (
+				<p>
+					<FiUpload />
+					Imagem do estabelecimento
+				</p>
+			)}
+		</div>
+	);
+};
 export default Dropzone;
--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -33,6 +33,8 @@ const CreatePoint = () => {
 	const [selectedUF, setSelectedUF] = useState<string>("0");
 	const [cities, setCities] = useState<string[]>([]);
 	const [selectedCity, setSelectedCity] = useState<string>("0");
+	const [selectedFile, setSelectedFile] = useState<File>();
+
 	const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
 		0, 0,
 	]);
@@ -135,17 +137,22 @@ const CreatePoint = () => {
 		const city = selectedCity;
 		const [latitude, longitude] = selectedPosition;
 		const items = selectedItems;
+
+		const data = new FormData();
 		
-		const data = {
-			name,
-			email,
-			whatsapp,
-			uf,
-			city,
-			latitude,
-			longitude,
-			items
-		}
+		
+			data.append('name', name)
+			data.append('email', email)
+			data.append('whatsapp', whatsapp)
+			data.append('uf', uf)
+			data.append('city', city)
+			data.append('latitude', String(latitude))
+			data.append('longitude', String(longitude))
+			data.append('items', items.join(','))
+			if (selectedFile) {
+				data.append('image', selectedFile)
+			}
+	
 
 		await api.post('points', data);
 
@@ -169,7 +176,7 @@ const CreatePoint = () => {
 					Cadastro do <br /> ponto de coleta
 				</h1>
 
-				<Dropzone />
+				<Dropzone onFileUploaded={setSelectedFile} />
 
 				<fieldset>
 					<legend>
