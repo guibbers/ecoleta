c129e3e3db0c73552addf774a1a7d22d6386243c
Guilherme Torres <guiatorres8@gmail.com>
2025-03-29 18:28:38 -0300

    add validation via celebrate

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -24,20 +25,22 @@ routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
 // Create collect point
-routes.post('/points', upload.single('image') , pointsController.create);
+routes.post('/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create);
 
 export default routes;
-
-// {   
-//   "name": "Mercadinho",
-//   "email": "contato@pomar.com.br",
-//   "whatsapp": "21999999999",
-//   "latitude": -22.903727,
-//   "longitude": -43.1013029,
-//   "city": "Niterói",
-//   "uf": "RJ",
-//   "items": [
-//       3,
-//       4
-//   ]
-//   }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from "express";
 import path from 'node:path';
 import routes from "./routes";
+import { errors } from 'celebrate'
 
 const app = express();
 const port = 3333;
@@ -10,7 +11,9 @@ app.use(cors({}));
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
